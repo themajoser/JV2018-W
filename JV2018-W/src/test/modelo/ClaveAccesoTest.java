c5db5bdc40f87ece79890be6b9d2f5ec788f356e
jorge <jorge@jorge-VirtualBox>
2019-05-06 15:55:34 +0200

    Nueva clase "ClaveAccesotest".

--- JV2018-W/src/test/modelo/ClaveAccesoTest.java
+++ JV2018-W/src/test/modelo/ClaveAccesoTest.java
@@ -2,7 +2,7 @@
  *  Implementa el concepto de claveAccesoTest según el modelo 2.1
  *  @since: prototipo2.1
  *  @source: ClaveAccesoTest.java 
- *  @version: 2.1 - 2019/04/29
+ *  @version: 2.1 - 2019/05/06
  *  @author: GRUPO 2:
  *  @author: Jorge Orenes Rubio
  */
